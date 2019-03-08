3ea6e819fd6dcd46e25c00a97a63d200e0a786bf
Amanjot <amanjot9kaur@gmail.com>
2019-03-08 18:21:01 -0500

    lab1

---
+++ DelegateAndEvents/DelegateAndEvents/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndEvents
+{
+   public class DelegateExercises
+    {
+
+    
+        public delegate void MyDelegate();
+    void Method1()
+    {
+        Console.WriteLine("Method1");
+        Console.ReadLine();
+    }
+    public void Method2()
+    {
+        MyDelegate myDelegate = new MyDelegate(Method1);
+        myDelegate();
+    }
+    
+       public static void Main(string[] args)
+        {
+            DelegateExercises
+                delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
+       }
+    }
+    
+}
