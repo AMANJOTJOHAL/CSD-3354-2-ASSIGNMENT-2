e8a99510b7cd0e54d79659a3e7818fc0fb5308e3
Amanjot <amanjot9kaur@gmail.com>
2019-03-08 19:38:10 -0500

    lab12

--- DelegateAndEvents/DelegateAndEvents/Program.cs
+++ DelegateAndEvents/DelegateAndEvents/Program.cs
@@ -6,17 +6,39 @@ using System.Threading.Tasks;
 
 namespace DelegateAndEvents
 {
+    using System;
 
+    delegate void ExampleDelegate(string xyz);
 
-    public delegate void MyDelegate();
-
-    public class DelegateExercises
+    class Program
     {
-        void Method3()
+        public static void Method1(string xyz)
         {
-            System.Console.WriteLine(myDelegate.ToString());
+            Console.WriteLine(xyz + " Method1");
+        }
 
+        public static void Method2(string xyz)
+        {
+            Console.WriteLine(xyz + " Method2");
+        }
 
+        public static void Main()
+        {
+            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
+
+            ex1Delegate = new ExampleDelegate(Method1);
+            ex2Delegate = new ExampleDelegate(Method2);
+            ex3Delegate = ex1Delegate + ex2Delegate;
+            myDelegate = ex1Delegate - ex2Delegate;
+            ex1Delegate("AAA");
+            ex2Delegate("BBB");
+            ex3Delegate("CCC");
+            myDelegate("DDD");
+            myDelegate = ex3Delegate - ex1Delegate;
+            myDelegate("EEE");
+            myDelegate = ex3Delegate - ex2Delegate;
+            myDelegate("FFF");
+            Console.ReadLine();
         }
     }
 }
